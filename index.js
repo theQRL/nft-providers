f3c3ca44eff23ebc8036a352a9eb34e5ac5c6d54
JP Lomas <jp@theqrl.org>
2022-02-18 13:56:16 +0000

    Add providers.yml and code generator

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 /* AUTO GENERATED FILE DO NOT EDIT OR MAKE PRs FOR THIS FILE */
 
-const qrlNft = {
-  version: "0.0.1",
-  providers: [
-    {
-      name: "theqrl.org",
-      addresses: [
-        "Q0105004f6ed1a0f36a7c4b77b87ff857f159ddc78be6aaf46b2d564415495bf9fb3b2295dce125",
-        "Q01050038ca2264a06ee154a681201f99079bcd1ddf28355060b969daed25dd98efbf9ffd61b9b6",
-      ],
-      url: "https://theqrl.org",
-      id: "0xbb44dd66",
-    },
-    {
-      name: "fakeexample.com",
-      addresses: [
-        "Q0105004f6ed1a0f36a7c4b77b87ff857f159ddc78be6aaf46b2d564415495bf9fb3b2295dce125",
-        "Q01050038ca2264a06ee154a681201f99079bcd1ddf28355060b969daed25dd98efbf9ffd61b9b6",
-      ],
-      url: "https://fake.com",
-      id: "0xffbbaa44",
-    },
-  ],
-};
+const qrlNft = {"version":"0.0.1", "providers":[{"name":"theqrl.org","addresses":["Q0105004f6ed1a0f36a7c4b77b87ff857f159ddc78be6aaf46b2d564415495bf9fb3b2295dce125","Q01050038ca2264a06ee154a681201f99079bcd1ddf28355060b969daed25dd98efbf9ffd61b9b6"],"url":"https://theqrl.org","id":"0x00001122"},{"name":"fakeexample.com","addresses":["Q0105004f6ed1a0f36a7c4b77b87ff857f159ddc78be6aaf46b2d564415495bf9fb3b2295dce125","Q01050038ca2264a06ee154a681201f99079bcd1ddf28355060b969daed25dd98efbf9ffd61b9b6"],"url":"https://fake.com","id":"0x00001123"}]};
 
 module.exports = qrlNft;
-
